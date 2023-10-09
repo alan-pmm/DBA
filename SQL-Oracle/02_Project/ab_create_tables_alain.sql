c8b2ca526935ce0cd3fe8c9b6a1dfc4c6345dc75
alan-pmm <55274117+alan-pmm@users.noreply.github.com>
2023-10-09 14:48:02 +0200

    create tables with few fields more

--- SQL-Oracle/02_Project/ab_create_tables_alain.sql
+++ SQL-Oracle/02_Project/ab_create_tables_alain.sql
@@ -1,3 +1,5 @@
+
+
 CREATE TABLE ALAIN.a_person
 (
 person_id NUMBER(*,0) NOT NULL,
@@ -12,16 +14,19 @@ CREATE TABLE ALAIN.a_product
 (
 product_id NUMBER(*,0) NOT NULL,
 product_name VARCHAR(50),
+product_size VARCHAR(50),
+product_codification  VARCHAR(50),
 CONSTRAINT pk_product_id PRIMARY KEY (product_id)
 )TABLESPACE ALAIN_SPACE 
 ;
 
 CREATE TABLE ALAIN.a_order_history
-   (	order_id NUMBER(*,0), 
+   (order_line_id NUMBER(*,0),
+    order_id NUMBER(*,0), 
     order_date TIMESTAMP (6), 
     product_id NUMBER(*,0), 
     person_id NUMBER(*,0),
-CONSTRAINT pk_order_id PRIMARY KEY (order_id)
+CONSTRAINT pk_order_line_id PRIMARY KEY (order_line_id)
 ) TABLESPACE ALAIN_SPACE
 ;
 
@@ -31,4 +36,3 @@ ADD CONSTRAINT fk_product_id foreign key("PRODUCT_ID") references "A_PRODUCT"("P
 ALTER  TABLE ALAIN.a_order_history
 ADD CONSTRAINT fk_person_id foreign key("PERSON_ID") references "A_PERSON"("PERSON_ID")
 ;
-
