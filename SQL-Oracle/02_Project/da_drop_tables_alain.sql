7019ad665f7bb04588a5b90f214bd96c89048419
alan-pmm <55274117+alan-pmm@users.noreply.github.com>
2023-10-24 16:09:07 +0200

    Delete SQL-Oracle/02_Project/da_drop_tables_alain.sql

--- SQL-Oracle/02_Project/da_drop_tables_alain.sql
+++
@@ -1,4 +0,0 @@
-
-drop table "ALAIN"."A_PRODUCT" cascade constraints PURGE;
-drop table "ALAIN"."A_PERSON" cascade constraints PURGE;
-drop table "ALAIN"."A_ORDER_HISTORY" cascade constraints PURGE;
