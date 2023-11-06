d964e603aba0daca6347e7bbfb819221e9c9a144
alan-pmm <55274117+alan-pmm@users.noreply.github.com>
2023-11-06 13:07:25 +0100

    Explain plan and autotrace exemple

--- SQL-Oracle/02_Project/ea_explain_plan_index_test_on_table_a_product.sql
+++ SQL-Oracle/02_Project/ea_explain_plan_index_test_on_table_a_product.sql
@@ -1,17 +1,18 @@
 
+--=======================================================================================
+-- RUN SEVERAL EXPLAIN SCENARIOS FROM SAME QUERY AND CREATE A VIEW USING THE TABLE 'plan_table'
+--
+--=======================================================================================
+--FLUSH plan_table
+DELETE FROM plan_table;
+
+
 --TEST 01 =======================================================================================
 EXPLAIN PLAN SET STATEMENT_ID = 'ckid01'
  --STORE THE EXPLAIN PLAN INTO THE TABLE 'plan_table'    
     INTO plan_table FOR
-    SELECT  product_name, product_size, COUNT(oh.order_line_id)  FROM a_product pd INNER JOIN a_order_history oh ON pd.product_id = oh.product_id
-    WHERE product_name = 'Socks' AND product_size LIKE '%S%'
-    GROUP BY product_name, product_size
-    ORDER BY 1 DESC;
+       SELECT  *  FROM a_product WHERE product_name = 'Trouser' AND product_size LIKE '%S%' ORDER BY 1 DESC;
        
-SET AUTOTRACE ON;
-   SELECT  *  FROM a_product WHERE product_name = 'Socks' AND product_size LIKE '%S%' ORDER BY 1 DESC;
-SET AUTOTRACE OFF;    
-
 
 --TEST 02 --TEST QUERY WITH 2 BITMAP INDEX ON a_product (product_name) a_product(product_size) ================================
 --CREATE BITMAP INDEX ON ROW HAVING LOW CARDINALITY (It MEANS FEW RECORDS DIFFERENCE)
@@ -21,62 +22,42 @@ SET AUTOTRACE OFF;
 EXPLAIN PLAN SET STATEMENT_ID = 'ckid02'
  --STORE THE EXPLAIN PLAN INTO THE TABLE 'plan_table'    
     INTO plan_table FOR
-    SELECT  product_name, product_size, COUNT(oh.order_line_id)  FROM a_product pd INNER JOIN a_order_history oh ON pd.product_id = oh.product_id
-    WHERE product_name = 'Socks' AND product_size LIKE '%S%'
-    GROUP BY product_name, product_size
-    ORDER BY 1 DESC;
+    SELECT  *  FROM a_product WHERE product_name = 'Trouser' AND product_size LIKE '%S%' ORDER BY 1 DESC;
 --DROP BITMAP INDEX    
     DROP INDEX idx_btm_a_product_1; 
     DROP INDEX idx_btm_a_product_2;
-
-SET AUTOTRACE ON;
-   SELECT  *  FROM a_product WHERE product_name = 'Socks' AND product_size LIKE '%S%' ORDER BY 1 DESC;
-SET AUTOTRACE OFF;    
+ 
 
 
-    
 --TEST 03 --TEST QUERY WITH A BITMAP INDEX ON a_product (product_name ASC, product_size) ================================
     CREATE BITMAP INDEX idx_btm_a_product_3 ON a_product (product_name ASC, product_size) TABLESPACE ALAIN_SPACE;
 --RUN EXPLAIN PLAN
 EXPLAIN PLAN SET STATEMENT_ID = 'ckid03'
  --STORE THE EXPLAIN PLAN INTO THE TABLE 'plan_table'    
     INTO plan_table FOR
-    SELECT  product_name, product_size, COUNT(oh.order_line_id)  FROM a_product pd INNER JOIN a_order_history oh ON pd.product_id = oh.product_id
-    WHERE product_name = 'Socks' AND product_size LIKE '%S%'
-    GROUP BY product_name, product_size
-    ORDER BY 1 DESC;
+    SELECT  *  FROM a_product WHERE product_name = 'Trouser' AND product_size LIKE '%S%' ORDER BY 1 DESC;
 --DROP BITMAP INDEX    
     DROP INDEX idx_btm_a_product_3; 
 
-SET AUTOTRACE ON;
-   SELECT  *  FROM a_product WHERE product_name = 'Socks' AND product_size LIKE '%S%' ORDER BY 1 DESC;
-SET AUTOTRACE OFF;        
-    
  --TEST 04 --TEST QUERY WITH A NONUNIQUE INDEX ON a_product (product_name ASC, product_size) ================================
     CREATE INDEX idx_btm_a_product_4 ON a_product (product_name ASC, product_size) TABLESPACE ALAIN_SPACE;
 --RUN EXPLAIN PLAN
 EXPLAIN PLAN SET STATEMENT_ID = 'ckid04'
  --STORE THE EXPLAIN PLAN INTO THE TABLE 'plan_table'    
     INTO plan_table FOR
-    SELECT  product_name, product_size, COUNT(oh.order_line_id)  FROM a_product pd INNER JOIN a_order_history oh ON pd.product_id = oh.product_id
-    WHERE product_name = 'Socks' AND product_size LIKE '%S%'
-    GROUP BY product_name, product_size
-    ORDER BY 1 DESC;
+     SELECT  *  FROM a_product WHERE product_name = 'Trouser' AND product_size LIKE '%S%' ORDER BY 1 DESC;
 --DROP BITMAP INDEX    
     DROP INDEX idx_btm_a_product_4;    
     
-SET AUTOTRACE ON;
-   SELECT  *  FROM a_product WHERE product_name = 'Socks' AND product_size LIKE '%S%' ORDER BY 1 DESC;
-SET AUTOTRACE OFF;    
 
-
+--CREATE OR REPLACE THE VIEW v_ea_explain_plan_with_bitmap_index_table_a_product
 CREATE OR REPLACE VIEW v_ea_explain_plan_with_bitmap_index_table_a_product AS
 SELECT statement_id, operation, options, object_name, optimizer,cost,cardinality,position,io_cost AS disq_read_io ,cpu_cost,time FROM plan_table
-WHERE statement_id LIKE 'ckid%';
+WHERE statement_id LIKE 'ckid%'ORDER BY statement_id DESC;
 
 CREATE OR REPLACE VIEW v_eb_explain_plan_with_bitmap_index_table_a_product_aggr AS
 SELECT statement_id AS st_id, SUM(cost) AS sum_cost
-, SUM(cardinality) AS sum_cardinality
+,SUM(cardinality) AS sum_cardinality
 ,SUM(position) AS sum_position,
 SUM(io_cost) AS sum_disq_read_io ,
 SUM(cpu_cost) AS sum_cpu_cost
@@ -84,9 +65,6 @@ FROM plan_table
 WHERE statement_id LIKE 'ckid%'
 GROUP BY st_id;
 
---SELECT * FROM table(dbms_xplan.display);
---SELECT * FROM table(dbms_xplan.display_cursor);
---SELECT * V$SQL_PLAN
 
 
 
